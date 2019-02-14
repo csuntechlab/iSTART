7d919b03b3f055e609b06bbf984e77622a577124
Andrew Grano <andrew.grano@metalab.csun.edu>
2019-02-14 13:30:05 -0800

    setup nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+const laravelNuxt = require("laravel-nuxt");
+
+module.exports = laravelNuxt({
+  // Options such as mode, srcDir and generate.dir are already handled for you.
+  modules: [],
+  plugins: [],
+});
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get(
+    '{uri}',
+    '\\'.Pallares\LaravelNuxt\Controllers\NuxtController::class
+)->where('uri', '.*');
