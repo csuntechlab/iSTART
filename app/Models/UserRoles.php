885850db9384aa309626f6e78768c9c08048be75
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-02-20 15:24:18 -0800

    created a Models folder. Sigh laravel 5.7 for not including this in their boilerplate

---
+++ app/Models/UserRoles.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserRoles extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_email', 'user_role',
+    ];
+}
