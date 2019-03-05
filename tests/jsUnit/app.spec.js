6eca37d7759017cc1c747263dcbb1f75071d169e
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-03-05 14:37:57 -0800

    Fixed test

--- tests/jsUnit/app.spec.js
+++ tests/jsUnit/app.spec.js
@@ -1,5 +1,11 @@
+import 'jest-extended'
+
 describe('setup test', () => {
   it('should add to 2', () => {
     expect(1 + 1).toBe(2)
+  })
+
+  it('should be even', () => {
+    expect(1 + 2).toBeOdd()
   })
 })
