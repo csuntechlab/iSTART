e31be61316ab5c1d699a028896d9cd3c0e5458d5
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-03-07 13:29:14 -0800

    Added tab accessability

--- resources/js/components/DashboardNotification.vue
+++ resources/js/components/DashboardNotification.vue
@@ -5,9 +5,11 @@
         <span class="dashboard-notification__message">Next module will be available on</span>
         <span class="dashboard-notification__message-date">00/00/00</span>
       </div>
-      <div class="dashboard-notification__schedule-button col-5 align-self-end">
-        <span class="dashboard-notification__schedule-text">Schedule</span>
-        <i class="fas fa-chevron-right"></i>
+      <div class="dashboard-notification__schedule-button pointer col-5 align-self-end">
+        <router-link to="/">
+          <span class="dashboard-notification__schedule-text">Schedule</span>
+          <i class="fas fa-chevron-right"></i>
+        </router-link>
       </div>
     </div>
   </div>
