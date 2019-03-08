24cbbad3c81ac24d47364a8c2e8949369c86dfaf
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-03-08 12:38:07 -0800

    I seem to have successfully revert all the tests i modified back to their original state, and have also successfully checked the functionality of the logout method in navbar.vue

--- tests/jsUnit/global/navbar.spec.js
+++ tests/jsUnit/global/navbar.spec.js
@@ -1,51 +1,14 @@
 import navbar from '../../../resources/js/globals/navbar'
-import Vuex from 'vuex'
-import User from '../../../resources/js/store/modules/user/index'
-import { shallowMount, createLocalVue } from '@vue/test-utils'
-
-const localVue = createLocalVue()
-localVue.use(Vuex)
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 
 describe('navbar.vue', () => {
-  let store
-  let state
-
-  state = {
-    user: {
-      token: '123rke4',
-      username: 'csunStudent123'
-    },
-    correctLoginInfo: true
-  }
-
-  beforeEach(() => {
-    store = new Vuex.Store({
-      modules: {
-        User: {
-          state: User.state,
-          actions: User.actions,
-          mutations: User.mutations
-        }
-      }
-    })
-  })
-  describe('user/actions/clearUserData', () => {
-    it('should have CLEAR_USER_DATA be called on logout as long as ', () => {
-      const navbarWrapper = shallowMount(navbar, { store, localVue })
+  describe('navbar.vue', () => {
+    it('should have logout method be called on click in [data-text=logout-button]', () => {
+      const logoutMock = jest.fn()
+      navbar.methods.logout = logoutMock
+      const navbarWrapper = shallowMount(navbar, {stubs: {RouterLink: RouterLinkStub }})
       navbarWrapper.find('[data-text=logout-button]').trigger('click')
-      const commit = jest.fn()
-      User.actions.clearUserData({ commit })
-      expect(commit).toHaveBeenCalledWith('CLEAR_USER_DATA')
-    })
-  })
-
-  describe('userStore/mutations/CLEAR_USER_DATA', () => {
-    it('should clear the user object and nullify correctLoginInfo when mutation is called', () => {
-      User.mutations.CLEAR_USER_DATA(state)
-      expect(state).toEqual({
-        user: {},
-        correctLoginInfo: null
-      })
+      expect(logoutMock).toHaveBeenCalled()
     })
   })
 })
