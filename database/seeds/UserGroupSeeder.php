7abeac89662a0a348e6a8a9b63c886c99dc8ee1c
ekno27 <edgkano@gmail.com>
2019-03-11 14:50:39 -0700

    changed userGroup seeder

--- database/seeds/UserGroupSeeder.php
+++ database/seeds/UserGroupSeeder.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 use Illuminate\Database\Seeder;
 
-class UserRoleSeeder extends Seeder
+class UserGroupSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run()
     {
-        DB::table('user_roles')->insert([
-            'user_role' => 'comparison',
-            'user_email' => 'nr_cyndee.bellamy@csun.edu',
+        DB::table('user_groups')->insert([
+            'user_group' => 'comparison',
+            'user_id' => 'nr_cyndee.bellamy@csun.edu',
             'display_name' => 'Cyndee Bellamy',
             'remember_token' => '12394iresjntgruirf9eiokwj',
         ]);
