cbbfc523e0c281f3d63cecd4a8a9b7e3e78109bd
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-03-14 14:15:01 -0700

    Created schedule component with mock data

---
+++ resources/js/components/schedule/schedule-modules.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="schedule-modules container-fluid">
+    <div v-for="(item, index) in schedule" :key="index" class="row">
+      <div class="col-7 col-l-6">
+        <span>{{ item.name }}</span>
+      </div>
+      <div class="schedule-modules__date col-5 col-l-6">
+        <span>{{ item.date }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ScheduleModules',
+  data () {
+    return {
+      schedule: {
+        Alcohol: {
+          name: 'Alcohol',
+          date: 'April 1, 2019'
+        },
+
+        Marijuana: {
+          name: 'Marijuana',
+          date: 'May 2, 2019'
+        },
+
+        Tobacco: {
+          name: 'Tobacco',
+          date: 'June 14, 2019'
+        },
+
+        LSD: {
+          name: 'LSD',
+          date: 'April 1, 2019'
+        }
+      }
+    }
+  }
+}
+</script>
---
+++ resources/js/views/schedule.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="schedule">
+    <div class="schedule-container container-fluid transition-350ms">
+      <div class="schedule-description">
+        <h1 class="schedule-description__header">Schedule</h1>
+        <p class="schedule-description__info">New modules, surveys, and quizzes will be available weekly.</p>
+      </div>
+
+      <div class="schedule-labels row">
+        <div class="col-5 col-l-6">
+          <span>Upcoming</span>
+        </div>
+        <div class="schedule-labels__available col-7 col-l-6">
+          <span>Date Available</span>
+        </div>
+      </div>
+      <hr class="schedule-divider">
+
+      <ScheduleModules></ScheduleModules>
+    </div>
+  </div>
+</template>
+
+<script>
+import ScheduleModules from './../components/schedule/schedule-modules'
+
+export default {
+  name: 'schedule',
+  components: {
+    ScheduleModules
+  }
+}
+</script>
