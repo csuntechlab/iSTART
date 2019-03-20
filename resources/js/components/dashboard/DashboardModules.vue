7e32cc21f1b59df22bb0a6aaeb9ab5df289a2326
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-03-20 10:33:28 -0700

    Updated styling

--- resources/js/components/dashboard/DashboardModules.vue
+++ resources/js/components/dashboard/DashboardModules.vue
@@ -2,7 +2,7 @@
   <router-link to="/">
     <div class="dashboard-modules">
       <div class="mt-5">
-        <div class="text-center m-0 col-12" v-for="(item, index) in modules" :key="index">
+        <div class="dashboard-modules-container text-center col-12" v-for="(item, index) in modules" :key="index">
           <div class="dashboard-modules__overview row">
             <div class="dashboard-modules__thumbnail">
               <img class="dashboard-modules__thumbnail-image" src="./../../../../public/images/thumbnail/test.jpg">
@@ -12,12 +12,12 @@
             </div>
           </div>
           <div class="dashboard-modules__progress row align-items-center">
-            <div class="col-4 col-md-6 dashboard-modules__percentage text-left">
-              <span>{{item.percentCompletion}}</span>
+            <div class="col-5 text-left">
+              <span>{{item.completion.date}}</span>
             </div>
-            <div class="col-8 col-md-6 dashboard-modules__completion text-right">
+            <div class="col-7 text-right">
               <span class="dashboard-modules__status">{{item.completion.status}}</span>
-              <i class="fas fa-chevron-right"></i>
+              <i v-bind:class="[ item.completion.completed ? 'dashboard-modules__status-circle fas fa-check-circle' : 'fas fa-chevron-right' ]"></i>
             </div>
           </div>
         </div>
@@ -38,23 +38,49 @@ export default {
       modules: {
         alcohol: {
           name: 'Alcohol',
-          percentCompletion: this.checkCompletion(12),
           completion: {
-            status: 'Incomplete',
-            date: '00/00/00'
+            progress: 0,
+            completed: false,
+            status: this.checkCompletionStatus(0),
+            date: 'Available until: 00/00/00'
           },
-          description: 'Alcohol'
+          description: 'Alcohol, I feel fuzzy'
+        },
+
+        marijuana: {
+          name: 'Marijuana',
+          completion: {
+            progress: 50,
+            completed: false,
+            status: this.checkCompletionStatus(50),
+            date: 'Available until: 00/00/00'
+          },
+          description: 'Marijuana; ...'
+        },
+
+        tobacco: {
+          name: 'Tobacco',
+          completion: {
+            progress: 100,
+            completed: true,
+            status: this.checkCompletionStatus(100),
+            date: 'Completed on: 00/00/00'
+          },
+          description: 'Tobacco; I feel relaxed'
         }
       }
     }
   },
 
   methods: {
-    checkCompletion (progressPercentAsNumber) {
-      if (progressPercentAsNumber < 1 || progressPercentAsNumber > 99) {
-        return ''
+    checkCompletionStatus (progressPercentAsNumber) {
+      console.log(progressPercentAsNumber)
+      if (progressPercentAsNumber < 1) {
+        return 'Start'
+      } else if (progressPercentAsNumber > 99) {
+        return 'Completed'
       } else {
-        return progressPercentAsNumber.toString() + '%'
+        return 'Continue'
       }
     }
   }
