420bbea84030e0dd0c8368968a0c72ca9c51266d
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-04-02 12:23:48 -0700

    Fixed BEM; Updated tags to be more semantic

--- resources/js/components/global/navbar.vue
+++ resources/js/components/global/navbar.vue
@@ -1,32 +1,32 @@
 <template>
-<div class="navbar-wrapper">
-  <nav class="navbar">
+<nav class="navbar-wrapper">
+  <div class="navbar">
     <button class="navbar__button" type="button" @click="toggleNavigation">
       <i class="fas fa-bars"></i>
     </button>
-  </nav>
-  <div id="dropdown" class="navbar__dropdown transition-350ms">
-    <ul class="navbar-nav navbar__dropdown-list">
-      <router-link to="/" class="navbar__dropdown-divider">
-        <div @click="closeNavigation" class="navbar__dropdown-item">
-          <li class="nav-link">MODULE</li>
-        </div>
-      </router-link>
-
-      <router-link to="/schedule" class="navbar__dropdown-divider">
-        <div class="navbar__dropdown-item" @click="closeNavigation">
-          <li class="nav-link">SCHEDULE</li>
-        </div>
-      </router-link>
-
-      <router-link to="/logout" class="navbar__dropdown-divider">
-        <div class="navbar__dropdown-item" @click="closeNavigation">
-          <li class="nav-link">LOGOUT</li>
-        </div>
-      </router-link>
-    </ul>
   </div>
-</div>
+  <div id="dropdown" class="navbar-dropdown transition-350ms">
+    <div class="navbar-nav navbar-dropdown-list">
+      <router-link to="/" class="navbar-dropdown-list__divider">
+        <div @click="closeNavigation" class="navbar-dropdown__item">
+          <span class="nav-link">MODULE</span>
+        </div>
+      </router-link>
+
+      <router-link to="/schedule" class="navbar-dropdown__divider">
+        <div class="navbar-dropdown__item" @click="closeNavigation">
+          <span class="nav-link">SCHEDULE</span>
+        </div>
+      </router-link>
+
+      <router-link to="/logout" class="navbar-dropdown__divider">
+        <div class="navbar-dropdown__item" @click="closeNavigation">
+          <span class="nav-link">LOGOUT</span>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script>
@@ -42,10 +42,10 @@ export default {
   methods: {
     toggleNavigation () {
       if (this.isNavOpen === false) {
-        document.getElementById('dropdown').classList.add('navbar__dropdown--show')
+        document.getElementById('dropdown').classList.add('navbar-dropdown--show')
         this.isNavOpen = true
       } else {
-        document.getElementById('dropdown').classList.remove('navbar__dropdown--show')
+        document.getElementById('dropdown').classList.remove('navbar-dropdown--show')
         this.isNavOpen = false
       }
     },
