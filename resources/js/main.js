1e59162f38cfdca887c0757be50c1dda9f426e47
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-04-03 12:21:33 -0700

    Removed unused service-worker

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,9 +3,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
-import './registerServiceWorker'
-import './../sass/app.scss'
 import VueProgress from 'vue-progress-path'
+import './../sass/app.scss'
 
 Vue.use(VueProgress)
 
