03c3f611a7c249bb36c9ca0ef739b1181868b1c5
ekno27 <edgkano@gmail.com>
2019-04-10 13:43:13 -0700

    reformatting and added button placeholders

--- resources/js/components/admin/Participants.vue
+++ resources/js/components/admin/Participants.vue
@@ -1,27 +1,7 @@
 <template>
   <div>
-    <div class="goodParticipants" v-if="categorizedParticipants.good !== undefined">
-      <h2> Good to go</h2>
-      {{categorizedParticipants}}
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">email</th>
-            <th scope="col">participant ID</th>
-            <th scope="col">user ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr :key="participant.participant_id" v-for="participant in categorizedParticipants.good">
-            <td>{{participant.email}}</td>
-            <td>{{participant.participant_id}}</td>
-            <td>{{participant.user_id}}</td>
-          </tr>
-        </tbody>    
-      </table>
-    </div>
     <div class="badParticipants" v-if="categorizedParticipants.bad !== undefined">
-      <h2>Bad to go</h2>
+      <h2>The following emails were not found: </h2>
       <table class="table table-striped" >
         <thead>
           <tr>
@@ -37,7 +17,27 @@
         </tbody>    
       </table>
     </div>
-    
+    <div class="goodParticipants" v-if="categorizedParticipants.good !== undefined">
+      <h2> The following emails can be submitted: </h2>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">email</th>
+            <th scope="col">participant ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr :key="participant.participant_id" v-for="participant in categorizedParticipants.good">
+            <td>{{participant.email}}</td>
+            <td>{{participant.participant_id}}</td>            
+          </tr>
+        </tbody>    
+      </table>
+      <p>Would you like submit the good participants?</p>
+
+      <button class="btn btn-danger">No</button>
+      <button class="btn btn-primary">Yes</button>
+    </div>
   </div>
 </template>
 <script>
