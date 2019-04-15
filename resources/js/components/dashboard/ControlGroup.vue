922e86f99091888d699b9ff706752ec0083d7aba
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-04-15 15:12:12 -0700

    pushing to see how it looks on other screens

--- resources/js/components/dashboard/ControlGroup.vue
+++ resources/js/components/dashboard/ControlGroup.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="control-group container">
-    <h1>Control Group</h1>
+<div class="control-group container">
+  <div class="row">
+    <div class="col-8 offset-2 text-center">
+      <div class="control-group__body">
+        <i class="control-group__icon fas fa-user" style="color:#299afb"></i>
+        <div class="control-group__content">
+          <p class="control-group__text">Thank you for participating.</p>
+          <p class="control-group__text">You are part of the control group.</p>
+          <p class="control-group__text">You will receive an email with further</p>
+          <p class="control-group__text">instructions.</p>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -4,38 +4,33 @@
       <div class="row">
         <h1 class="login__title transition-350ms">iSTART</h1>
       </div>
-    <div >
-      <div class="row justify-content-center">
-        <div class="col-12 col-sm-10 col-md-8 col-lg-6">
-          
-          <form class="loginForm">
-            <h2 class="loginForm__title">Login</h2>
-            <div class="form-group">
-              <label for="username" class="loginForm__label">CSUN Username or Email</label>
-              <input v-model="userInfoForm.username" type="username" class="form-control form-control-lg"
-              placeholder="CSUN Username or Email"
-              required>
-            </div>
-            <div v-if="submitted && !userInfoForm.username" class="invalid-feedback ">Please enter your credentials</div>
-            <div class="form-group">
-              <label for="password" class="loginForm__label">Password</label>
-              <input v-model="userInfoForm.password" type="password" class=" form-control form-control-lg" placeholder="Password" required>
-            </div>
-            <!-- <div v-if="submitted && !userInfoForm.password" class="invalid-feedback">Please Enter your password</div> -->
-            <div v-if="correctLoginInfo===false" class="invalid-feedback mt-0 mb-2 text-center">Login failed. Make sure you have the correct access rights.</div>
-            <div class="form-group text-center">
-              <button type="submit" @click.prevent="submitForm" class="btn btn-primary btn-lg">Sign In</button>
-            </div>
-          </form>
+      <div >
+        <div class="row justify-content-center">
+          <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+            <form class="loginForm">
+              <h2 class="loginForm__title">Login</h2>
+              <div class="form-group">
+                <label for="username" class="loginForm__label">CSUN Username or Email</label>
+                <input v-model="userInfoForm.username" type="username" class="form-control form-control-lg"
+                placeholder="CSUN Username or Email"
+                required>
+              </div>
+              <div v-if="submitted && !userInfoForm.username" class="invalid-feedback ">Please enter your credentials</div>
+              <div class="form-group">
+                <label for="password" class="loginForm__label">Password</label>
+                <input v-model="userInfoForm.password" type="password" class=" form-control form-control-lg" placeholder="Password" required>
+              </div>
+              <!-- <div v-if="submitted && !userInfoForm.password" class="invalid-feedback">Please Enter your password</div> -->
+              <div v-if="correctLoginInfo===false" class="invalid-feedback mt-0 mb-2 text-center">Login failed. Make sure you have the correct access rights.</div>
+              <div class="form-group text-center">
+                <button type="submit" @click.prevent="submitForm" class="btn btn-primary btn-lg">Sign In</button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-      
-    </div>
-    
   </div>
-
-  </div>
-  
 </template>
 
 <script>
