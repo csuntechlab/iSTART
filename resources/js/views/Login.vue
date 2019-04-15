63d03d018d24c7e57621a6e0c7db9ca6f9a9b174
ekno27 <edgkano@gmail.com>
2019-04-15 15:16:01 -0700

    styled correctly

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -7,31 +7,31 @@
     <div >
       <div class="row justify-content-center">
         <div class="col-12 col-sm-10 col-md-8 col-lg-6">
-          <form v-bind:class="[this.loginIsLoading? 'loginForm loginForm__loading' : 'loginForm']">
+          <form class="loginForm">
             <loading-progress
             class="loading-form"
             v-if="loginIsLoading"
             :progress=".30"
             :indeterminate="true"
-            :size="64"
+            :size="70"
             />
-            <div v-else>
+            <div v-bind:class="{'loginForm__loading':loginIsLoading}">
               <h2 class="loginForm__title">Login</h2>
               <div class="form-group">
                 <label for="username" class="loginForm__label">CSUN Username or Email</label>
                 <input v-model="userInfoForm.username" type="username" class="form-control form-control-lg"
-                placeholder="CSUN Username or Email"
+                placeholder="CSUN Username or Email" :disabled="loginIsLoading ? true: false"
                 required>
               </div>
               <div v-if="submitted && !userInfoForm.username" class="invalid-feedback ">Please enter your credentials</div>
               <div class="form-group">
                 <label for="password" class="loginForm__label">Password</label>
-                <input v-model="userInfoForm.password" type="password" class=" form-control form-control-lg" placeholder="Password" required>
+                <input v-model="userInfoForm.passwordd" type="password" class=" form-control form-control-lg" placeholder="Password"  :disabled="loginIsLoading ? true: false" required>
               </div>
               <!-- <div v-if="submitted && !userInfoForm.password" class="invalid-feedback">Please Enter your password</div> -->
               <div v-if="correctLoginInfo===false" class="invalid-feedback mt-0 mb-2 text-center">Login failed. Make sure you have the correct access rights.</div>
               <div class="form-group text-center">
-                <button type="submit" @click.prevent="submitForm" class="btn btn-primary btn-lg">Sign In</button>
+                <button type="submit" @click.prevent="submitForm" class="btn btn-primary btn-lg" :disabled="loginIsLoading">Sign In</button>
               </div>
             </div>
           </form>
