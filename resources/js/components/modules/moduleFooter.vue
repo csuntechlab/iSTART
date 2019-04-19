5a0d94d9df0fee9d9f91636ec6b8b5ce7f468a51
Josh <joshua.magdaleno.472@my.csun.edu>
2019-04-19 15:57:39 -0700

    made it look like da mockups. It works on refresh but it is not slick with its mobile responsivity

--- resources/js/components/modules/moduleFooter.vue
+++ resources/js/components/modules/moduleFooter.vue
@@ -1,13 +1,11 @@
 <template>
-<div class="module__footer">
-  <div class="row">
+  <div class="module-footer__background row">
       <div class="col-6 text-left">
         <router-link :class="width >= 576 ? 'btn btn-primary' : 'module__footerText module__footerText--left'" to="/"><i class="fas fa-chevron-left"></i> Go Back</router-link>
       </div>
       <div class="col-6 text-right">
         <router-link :class="width >= 576 ? 'btn btn-primary' : 'module__footerText module__footerText--right'" to="/module">Continue <i class="fas fa-chevron-right"></i></router-link>
       </div>
-    </div>
   </div>
 </template>
 <script>
