db84cbcfcc4976395e6aec2e7113908088d4dfc3
Tiffany <n0isehazard@gmail.com>
2019-04-19 11:37:13 -0700

    Revert "STR-106 -- FE: Create Actual Control group page"

--- resources/js/components/dashboard/ControlGroup.vue
+++ resources/js/components/dashboard/ControlGroup.vue
@@ -1,19 +1,7 @@
 <template>
-<div class="control-group container">
-  <div class="row">
-    <div class="col-8 offset-2 text-center">
-      <div class="control-group__body">
-        <i class="control-group__icon fas fa-user"></i>
-        <div class="mt-3">
-          <p class="control-group__text">Thank you for participating.<br>
-          You are part of the control group.<br>
-          You will receive an email with further<br>
-          instructions.</p>
-        </div>
-      </div>
-    </div>
+  <div class="control-group container">
+    <h1>Control Group</h1>
   </div>
-</div>
 </template>
 
 <script>
