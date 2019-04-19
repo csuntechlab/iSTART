cb2ac9fd538b23aa0df4e7baf303b691c687a561
Josh <joshua.magdaleno.472@my.csun.edu>
2019-04-19 15:28:36 -0700

    changing git accounts

--- resources/js/components/modules/moduleFooter.vue
+++ resources/js/components/modules/moduleFooter.vue
@@ -1,11 +1,13 @@
 <template>
-<div class="row">
+<div class="module__footer">
+  <div class="row">
       <div class="col-6 text-left">
-        <router-link :class="width >= 768 ? 'btn btn-primary' : 'module__footerText module__footerText--left'" to="/"><i class="fas fa-chevron-left"></i> Go Back</router-link>
+        <router-link :class="width >= 576 ? 'btn btn-primary' : 'module__footerText module__footerText--left'" to="/"><i class="fas fa-chevron-left"></i> Go Back</router-link>
       </div>
       <div class="col-6 text-right">
-        <router-link :class="width >= 768 ? 'btn btn-primary' : 'module__footerText module__footerText--right'" to="/module">Continue <i class="fas fa-chevron-right"></i></router-link>
+        <router-link :class="width >= 576 ? 'btn btn-primary' : 'module__footerText module__footerText--right'" to="/module">Continue <i class="fas fa-chevron-right"></i></router-link>
       </div>
+    </div>
   </div>
 </template>
 <script>
