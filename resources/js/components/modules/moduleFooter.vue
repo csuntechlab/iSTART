12de848582d57b754564033fed0d81b25e00c762
Josh <joshua.magdaleno.472@my.csun.edu>
2019-04-19 17:55:51 -0700

    final changes for the week

--- resources/js/components/modules/moduleFooter.vue
+++ resources/js/components/modules/moduleFooter.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="module-footer__background row">
-      <div class="col-6 text-left">
-        <router-link :class="width >= 576 ? 'btn btn-primary' : 'module__footerText module__footerText--left'" to="/"><i class="fas fa-chevron-left"></i> Go Back</router-link>
+      <div :class="width >= 576 ? 'text-right' : 'text-left'" class="col-6">
+        <router-link :class="width >= 576 ? 'btn btn-primary mr-3' : 'module__footerText module__footerText--left text-left'" to="/"><i v-show="width<=576" class="fas fa-chevron-left"></i> Go Back</router-link>
       </div>
-      <div class="col-6 text-right">
-        <router-link :class="width >= 576 ? 'btn btn-primary' : 'module__footerText module__footerText--right'" to="/module">Continue <i class="fas fa-chevron-right"></i></router-link>
+      <div :class="width>= 576 ? 'text-left' : 'text-right'" class="col-6">
+        <router-link :class="width >= 576 ? 'btn btn-primary ml-3' : 'module__footerText module__footerText--right text-right'" to="/module">Continue <i v-show="width<=576" class="fas fa-chevron-right"></i></router-link>
       </div>
   </div>
 </template>
