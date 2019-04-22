54d1846013352ecd694e60abf92c2dfdb2d69694
mbendandour <mbenda.ndour@metalab.csun.edu>
2019-04-22 14:42:01 -0700

    change email variables in order to fix login issues

--- resources/views/mail/generic.blade.php
+++ resources/views/mail/generic.blade.php
@@ -1,4 +1,4 @@
 @component('mail::message')
 Hello Admin,
     User group is: {{$genericEmail->user_group}}
-    User Participant ID is: {{$genericEmail->Participant_id}}
+    User Participant ID is: {{$genericEmail->participant_id}}
