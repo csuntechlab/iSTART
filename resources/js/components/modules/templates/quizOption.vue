e6e1a01b65d2387d0fd5537efcf3f707113c0d01
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-05-01 17:03:53 -0700

    creating a card that holds images and text. However I need i broke something and I don't know how

--- resources/js/components/modules/templates/quizOption.vue
+++ resources/js/components/modules/templates/quizOption.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="col-sm-6 col-12">
-    <div v-on:click="newSlide()" class="module-quiz__option">
-      <div v-if="getAccessToDisplayContent && option_number == answer" class="module-quiz__selection">
+    <div v-if="image" v-on:click="newSlide()" class="module-quiz__option module-quiz__option--img">
+      <div v-if="getAccessToDisplayContent && optionID == answer" class="module-quiz__selection--img">
+        <i class="fas fa-check-circle module-quiz__icon"></i>
+      </div>
+      <div v-else-if="getAccessToDisplayContent && selectedCard > 0" class="module-quiz__selection--img">
+        <i class="fas fa-times-circle module-quiz__icon"></i>
+      </div>
+      <img class="module-quiz__img" :src="image" alt="alcohol module imgae">
+      <p class="module-quiz__text">Option {{ optionID }}: Lorem, ipsum dolor sit amet consectetur</p>
+    </div>
+    <div v-else v-on:click="newSlide()" class="module-quiz__option">
+      <div v-if="getAccessToDisplayContent && optionID == answer" class="module-quiz__selection">
         <i class="fas fa-check-circle module-quiz__icon"></i>
       </div>
       <div v-else-if="getAccessToDisplayContent && selectedCard > 0" class="module-quiz__selection">
         <i class="fas fa-times-circle module-quiz__icon"></i>
       </div>
-      <p class="module-quiz__text">Option {{ option_number }}: Lorem, ipsum dolor sit amet consectetur adipisicing elitfficiis! Consequuntur,  </p>
+      <img class="module-quiz__img" :src="image" alt="alcohol module imgae">
+      <p class="module-quiz__text">Option {{ optionID }}: Lorem, ipsum dolor sit amet consectetur adipisicing elitfficiis! Consequuntur,  </p>
     </div>
   </div>
 </template>
@@ -16,13 +27,15 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'quizOption',
   props: [
-    'option_number',
-    'optionID'
+
+    'optionID',
+    'image'
   ],
   data () {
     return {
       answer: 3,
       selectedCard: 0
+
     }
   },
   computed: {
--- resources/js/components/modules/templates/quizTemplate.vue
+++ resources/js/components/modules/templates/quizTemplate.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="row mt-4 justify-content-center">
       <quiz-option v-for="(option, id)  in options"
-        :option_number="option"
+        :image="option.image"
         :key="id"
         :optionID="id">
       </quiz-option>
@@ -26,7 +26,20 @@ export default {
   },
   data () {
     return {
-      options: [1, 2, 3, 4]
+      options: {
+        0: {
+          image: 'images/thumbnail/alcohol_thumbnail.jpg'
+        },
+        1: {
+          image: 'images/thumbnail/alcohol_thumbnail.jpg'
+        },
+        2: {
+          image: 'images/thumbnail/alcohol_thumbnail.jpg'
+        },
+        3: {
+          image: 'images/thumbnail/alcohol_thumbnail.jpg'
+        }
+      }
     }
   }
 }
