6e425a656267addadd6adb86e0b99451ebff896b
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-05-13 15:28:59 -0700

    creating a component to take in more data

---
+++ resources/js/components/modules/templates/infoContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="module-info__sentence"><i :class="icon.c"></i> {{ paragraph }}</p>
+</template>
+
+<script>
+export default {
+  name: 'infoContent',
+  props: [
+    'paragraph',
+    'icon'
+  ]
+}
+</script>
--- resources/js/components/modules/templates/infoTemplate.vue
+++ resources/js/components/modules/templates/infoTemplate.vue
@@ -8,14 +8,23 @@
     <div class="row">
       <div class="col-12">
         <div class="module-info__body">
-          <p class="module-info__sentence" v-for="(sentence, key) in content" :key="key"><i :class="sentence.icon.c"></i> {{ sentence.paragraph }}</p>
+          <info-content v-for="(element, index) in content"
+            :key="index"
+            :paragraph="element.paragraph"
+            :icon="element.icon">
+          </info-content>
+          <!-- <p class="module-info__sentence" v-for="(sentence, key) in content" :key="key"><i :class="sentence.icon.c"></i> {{ sentence.paragraph }}</p> -->
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import infoContent from './infoContent'
 export default {
+  components: {
+    infoContent
+  },
   data () {
     return {
       content: {
@@ -45,6 +54,30 @@ export default {
         },
         4: {
           paragraph: 'Dawg why you drinking',
+          icon: {
+            'c': 'fas fa-dizzy', 'l': 'dizzy'
+          }
+        },
+        5: {
+          paragraph: 'Getting your BAC levels above 0.8 can get you a ticket',
+          icon: {
+            'c': 'fas fa-dizzy', 'l': 'dizzy'
+          }
+        },
+        6: {
+          paragraph: 'Ya mama gon be mad',
+          icon: {
+            'c': 'fas fa-dizzy', 'l': 'dizzy'
+          }
+        },
+        7: {
+          paragraph: '1 beer is equivalent to a shot of tequila',
+          icon: {
+            'c': 'fas fa-dizzy', 'l': 'dizzy'
+          }
+        },
+        8: {
+          paragraph: 'Always have a DD if you drinking',
           icon: {
             'c': 'fas fa-dizzy', 'l': 'dizzy'
           }
