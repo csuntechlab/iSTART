bebe3bec63af46600fa27e6af61df0d0a0dbcdb4
mbendandour <mbenda.ndour@metalab.csun.edu>
2019-05-13 10:48:39 -0700

    Things dissapeared for some reason. Adding missing items

--- app/Contracts/ModuleProgressContract.php
+++ app/Contracts/ModuleProgressContract.php
@@ -7,5 +7,5 @@ interface ModuleProgressContract
     public function getModuleProgress($data): array;
 
     public function setModuleProgress($data);
-    public function remindUserofModule($request);
+
 }
