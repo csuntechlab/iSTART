0ff8daf90bdf6f1ee7a18f440342a563e4d96891
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-05-15 14:26:35 -0700

    fixed tests

--- tests/jsUnit/components/modules/template.spec.js
+++ tests/jsUnit/components/modules/template.spec.js
@@ -1,9 +1,9 @@
 import slideStore from './../../../../resources/js/store/modules/slides/'
 
-describe('slideStore/actions/toggleContent', () => {
-  it('should commit to SHOWS_OR_HIDE_CONTENT mutation when action is called', () => {
+describe('slideStore/actions/allowUserToContinue', () => {
+  it('should commit to ENABLE_OR_DISABLE_CONTINUE mutation when action is called', () => {
     let commit = jest.fn()
-    slideStore.actions.toggleContent({ commit }, true)
-    expect(commit).toHaveBeenCalledWith('SHOWS_OR_HIDES_CONTENT', true)
+    slideStore.actions.allowUserToContinue({ commit }, true)
+    expect(commit).toHaveBeenCalledWith('ENABLE_OR_DISABLE_CONTINUE', true)
   })
 })
