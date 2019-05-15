e4b5ef93a80cfa09d824ed6fa78b31668c297698
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-05-15 15:09:48 -0700

    Adjusted padding and margins

--- resources/js/components/modules/moduleContent.vue
+++ resources/js/components/modules/moduleContent.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="row">
-        <div class="col-12">
-          <h1>Title of Video</h1>
-        </div>
-        <div class="module-content col-12">
-          <youtube :video-id="videoId" :width="videoWidth" @ended="test" :player-vars="{autoplay: 1, modestbranding: 1, rel: 0, cc_load_policy: 1, controls: 0, disablekb: 1}"></youtube>
-        </div>
-        <div class="col-12">
-          <caption>Binge drinking is bad.</caption>
-        </div>
+  <div class="video-template row">
+    <div class="video-template__title col-12">
+      <h1>Binge Drinking</h1>
     </div>
+    <div class="video-template__video col-12">
+      <youtube :video-id="videoId" player-width="100%" @ended="test" :player-vars="{autoplay: 1, modestbranding: 1, rel: 0, cc_load_policy: 1, controls: 0, disablekb: 1}"></youtube>
+    </div>
+    <div class="video-template__caption col-12">
+      <caption>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus risus vitae ullamcorper fringilla. Donec consectetur neque quam.</caption>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,8 +18,7 @@ export default {
 
   data () {
     return {
-      videoId: '5jNEVGj6gS0',
-      videoWidth: '100%'
+      videoId: '5jNEVGj6gS0'
     }
   },
 
