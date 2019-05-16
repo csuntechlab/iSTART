df018006c48e2ced4dbf16e54d401c3179e2da7a
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-05-16 11:17:37 -0700

    Added flip counter to check against total cards

--- resources/js/components/modules/templates/cardFlip.vue
+++ resources/js/components/modules/templates/cardFlip.vue
@@ -6,7 +6,7 @@
   </section>
   <div class="card-flip-content">
     <div class="card-flip__card col-12 col-md-6 col-lg-4" v-for="(item, index) in slides.slide1.content.cards" :key="index">
-      <div @click="item.show=!item.show" v-bind:class="[item.show ? 'card-flip__card--active' : '', 'card-flip__card-wrapper pointer transition-350ms']">
+      <div @click="item.show=!item.show; item.show ? updateCount(1) : updateCount(-1)" v-bind:class="[item.show ? 'card-flip__card--active' : '', 'card-flip__card-wrapper pointer transition-350ms']">
         <div class="card-flip__card-front">
           <figure v-if="item.front.img.src!=null">
             <img v-bind:src="item.front.img.src" v-bind:alt="item.front.img.alt" class="card-flip__card-image">
@@ -31,6 +31,21 @@ import { alcoholSlide } from './../data/alcoholSlide.js'
 export default {
   mixins: [
     alcoholSlide
-  ]
+  ],
+
+  data () {
+    return {
+      cardFlipCount: 0
+    }
+  },
+
+  methods: {
+    updateCount (number, item) {
+      this.cardFlipCount += number
+      if (this.cardFlipCount === Object.keys(this.slides.slide1.content.cards).length) {
+        // call continue action
+      }
+    }
+  }
 }
 </script>
