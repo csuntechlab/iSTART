4356024282865b748fdf16448cf6481854cd68a7
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-05-22 17:07:44 -0700

    Added video data; Implemented template

--- resources/js/components/modules/templates/videoTemplate.vue
+++ resources/js/components/modules/templates/videoTemplate.vue
@@ -1,29 +1,38 @@
 <template>
   <div class="video-template justify-content-center mb-5 row">
     <div class="video-template__title col-12">
-      <h1>Binge Drinking</h1>
+      <h1>{{ current_slide(slideNumber).header.title }}</h1>
+      <p>{{ current_slide(slideNumber).header.text }}</p>
     </div>
     <div class="video-template__video col-12 col-md-10">
       <div class="embed-responsive embed-responsive-16by9">
-        <youtube :video-id="videoId" player-width="100%" @ended="newSlide" :player-vars="{autoplay: 1, modestbranding: 1, rel: 0, cc_load_policy: 1, controls: 0, disablekb: 1}"></youtube>
+        <youtube :video-id="current_slide(slideNumber).content.video_id" player-width="100%" @ended="proceedAndContinue" :player-vars="{autoplay: 1, modestbranding: 1, rel: 0, cc_load_policy: 1, controls: 0, disablekb: 1}"></youtube>
       </div>
     </div>
     <div class="video-template__caption col-12">
-      <caption>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus risus vitae ullamcorper fringilla. Donec consectetur neque quam.</caption>
+      <caption>{{ current_slide(slideNumber).content.caption }}</caption>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'moduleContent',
 
-  data () {
-    return {
-      videoId: '5jNEVGj6gS0'
-    }
+  computed: {
+    ...mapState(
+      {
+        slideNumber: state => state.Slides.slide_index,
+        slides: state => state.Slides.importedJSONSlides
+      }
+    ),
+    ...mapGetters(
+      [
+        'current_slide'
+      ]
+    )
   },
 
   methods: {
@@ -32,8 +41,9 @@ export default {
         'allowUserToContinue'
       ]
     ),
-    newSlide: function () {
-      this.allowUserToContinue(true)
+
+    proceedAndContinue () {
+      this.allowUserToContinue({ isAbleToProceed: true, slide_index: this.slideNumber })
     }
   }
 }
