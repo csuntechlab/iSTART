7155dad65f1048f196fdab8e9d8e761546e0dcb5
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-05-24 14:55:00 -0700

    Implement card flip slides

--- resources/js/components/modules/templates/cardFlipTemplate.vue
+++ resources/js/components/modules/templates/cardFlipTemplate.vue
@@ -1,23 +1,26 @@
 <template>
 <div class="card-flip">
   <section class="card-flip-description container">
-    <h1>{{content.header}}</h1>
-    <p>{{content.description}}</p>
+    <h1>{{ current_slide(slideNumber).header.title }}</h1>
+    <p>{{ current_slide(slideNumber).header.text }}</p>
+    <div v-for="(item, index) in current_slide(slideNumber).content.paragraph" :key="index">
+      <p v-html="item"></p>
+    </div>
   </section>
   <div class="card-flip-content mb-5">
-    <div class="card-flip__card col-12 col-md-6 col-lg-4" v-for="(item, index) in content.cards" :key="index">
-      <div @click="item.show=!item.show; item.show ? updateCount(1) : updateCount(-1)" v-bind:class="[item.show ? 'card-flip__card--active' : '', 'card-flip__card-wrapper pointer transition-350ms']">
+    <div class="card-flip__card col-12 col-md-6 col-lg-4" v-for="(item, index) in current_slide(slideNumber).content.cards" :key="index">
+      <div @click="flipCard(index, item.show); item.show ? updateCount(1) : updateCount(-1)" v-bind:class="[item.show ? 'card-flip__card--active' : '', 'card-flip__card-wrapper pointer transition-350ms']">
         <div class="card-flip__card-front">
           <figure v-if="item.front.img.src!=null">
+            <figcaption class="card-flip__card-caption">{{item.front.img.caption}}</figcaption>
             <img v-bind:src="item.front.img.src" v-bind:alt="item.front.img.alt" class="card-flip__card-image">
-            <figcaption>{{item.front.img.caption}}</figcaption>
           </figure>
           <section v-if="item.front.question!=null">
             {{item.front.question}}
           </section>
         </div>
         <div class="card-flip__card-back">
-          <h1>{{item.back.description}}</h1>
+          <h1 v-html="item.back.description"></h1>
         </div>
       </div>
     </div>
@@ -26,74 +29,49 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters, mapState } from 'vuex'
 
 export default {
   data () {
     return {
-      cardFlipCount: 0,
-      content: {
-        header: 'What you shouldn\'t take drugs',
-        description: 'Flip over all cards to continue.',
-        cards: {
-          0: {
-            show: false,
-            front: {
-              img: {
-                src: 'images/thumbnail/test.jpg',
-                alt: 'city with palmtrees',
-                caption: 'California Sunshine'
-              },
-              question: null
-            },
-            back: {
-              description: 'Don\'t do drugs.'
-            }
-          },
-          1: {
-            show: false,
-            front: {
-              img: {
-                src: null,
-                alt: null,
-                caption: null
-              },
-              question: 'Why you shouldn\'t do drugs?'
-            },
-            back: {
-              description: 'Drugs are bad.'
-            }
-          },
-          2: {
-            show: false,
-            front: {
-              img: {
-                src: 'images/thumbnail/alcohol_thumbnail.jpg',
-                alt: 'luxury alcohol with glass',
-                caption: 'Alcohol with shots of whiskey'
-              },
-              question: null
-            },
-            back: {
-              description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac pretium tortor, vitae aliquam urna. Vivamus commodo suscipit risus, sed sagittis tellus. Nam at tortor cursus, semper risus ac, lacinia odio. Morbi vitae mollis diam.'
-            }
-          }
-        }
-      }
+      cardFlipCount: 0
     }
+  },
+
+  computed: {
+    ...mapState(
+      {
+        slideNumber: state => state.Slides.slide_index,
+        slides: state => state.Slides.importedJSONSlides
+      }
+    ),
+    ...mapGetters(
+      [
+        'current_slide'
+      ]
+    )
   },
 
   methods: {
     ...mapActions(
       [
-        'allowUserToContinue'
+        'allowUserToContinue',
+        'updateCard'
       ]
     ),
 
+    flipCard (itemIndex, isFlipped) {
+      this.updateCard({ currentSlideIndex: parseInt(this.slideNumber), currentCardIndex: parseInt(itemIndex), isFlipped: isFlipped })
+    },
+
+    proceedAndContinue () {
+      this.allowUserToContinue({ isAbleToProceed: true, slide_index: this.slideNumber })
+    },
+
     updateCount (number, item) {
       this.cardFlipCount += number
-      if (this.cardFlipCount === Object.keys(this.content.cards).length) {
-        this.allowUserToContinue(true)
+      if (this.cardFlipCount === Object.keys(this.current_slide(this.slideNumber).content.cards).length) {
+        this.proceedAndContinue()
       }
     }
   }
--- resources/js/store/modules/slides/actions.js
+++ resources/js/store/modules/slides/actions.js
@@ -3,25 +3,29 @@ export default {
   getSlideInfo ({ commit }, payload) {
     commit('GET_SLIDE_INFO_FROM_JSON', payload)
   },
+
+  updateCard ({ commit }, payload) {
+    commit('UPDATE_CARD', payload)
+  },
+
   allowUserToContinue ({ commit }, payload) {
     commit('ENABLE_OR_DISABLE_CONTENT', payload.isAbleToProceed)
     if (payload.slide_index > -1) {
       commit('ITERATE_THROUGH_CONTENT', payload.slide_index)
     }
   },
-  async emailSurveyResults({commit}, payload) {
+
+  async emailSurveyResults ({ commit }, payload) {
     return SlidesAPI.emailSurveyResultsAPI(payload)
-    .then(
-      response => {
-        commit('SURVEY_RESULTS_EMAIL_WAS_SUBMITTED', true)
-      })
-    .catch(
-      failure=> {
-        console.error(failure)
-        commit('SURVEY_RESULTS_EMAIL_WAS_SUBMITTED', false)
-        
-      }
-    )
-    
+      .then(
+        response => {
+          commit('SURVEY_RESULTS_EMAIL_WAS_SUBMITTED', true)
+        })
+      .catch(
+        failure => {
+          console.error(failure)
+          commit('SURVEY_RESULTS_EMAIL_WAS_SUBMITTED', false)
+        }
+      )
   }
 }
