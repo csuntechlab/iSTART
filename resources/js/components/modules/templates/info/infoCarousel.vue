cc2be0741e8a022a149b8c3a11f83b78cd399e6a
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-05-29 11:50:39 -0700

    Abstracted info carousel to seperate component

---
+++ resources/js/components/modules/templates/info/infoCarousel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div :class="['module-info__carousel-item carousel-item', ( index === '0' ? 'active' : '' )]" v-for="(element, index) in images" :key="index">
+        <img class="d-block w-100" :src="element">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'images'
+  ]
+}
+</script>
--- resources/js/components/modules/templates/infoTemplate.vue
+++ resources/js/components/modules/templates/infoTemplate.vue
@@ -31,31 +31,21 @@
         </info-photo>
       </div>
       <div v-if="current_slide(slideNumber).format.images && current_slide(slideNumber).format.images ==='carousel'">
-        <div id="carousel" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div :class="['module-info__carousel-item carousel-item', ( index === '0' ? 'active' : '' )]" v-for="(element, index) in current_slide(slideNumber).content.images" :key="index">
-              <img class="d-block w-100" :src="element">
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
+        <info-carousel :images="current_slide(slideNumber).content.images"></info-carousel>
       </div>
     </div>
   </div>
 </template>
+
 <script>
-import infoContent from './info/infoContent'
-import infoPhoto from './info/infoPhoto'
 import { mapState, mapGetters, mapActions } from 'vuex'
+import infoContent from './info/infoContent'
+import infoCarousel from './info/infoCarousel'
+import infoPhoto from './info/infoPhoto'
+
 export default {
   components: {
+    infoCarousel,
     infoContent,
     infoPhoto
   },
