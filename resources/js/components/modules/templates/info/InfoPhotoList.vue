103c0531f0389bc4d7b58918aa73a1ee071739a6
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-06-27 14:27:15 -0700

    inject css styling in the head of HTML document

--- resources/js/components/modules/templates/info/InfoPhotoList.vue
+++ resources/js/components/modules/templates/info/InfoPhotoList.vue
@@ -2,30 +2,55 @@
     <ul id="image-list">
         <li>Hi</li>
         <li>Hi</li>
-        <li>Hi</li>
+        <li>{{title}}</li>
     </ul>
 </template>
 
 <script>
 export default {
+  name: 'InfoPhotoList',
+  data () {
+    return {
+      title: 'hello',
+      photoList: {
+        // 0: {
+        //   text: 'Eat before you drink.',
+        //   image: 'images/alcoholModule/alcohol_s23_01.jpg',
+        // },
+        // 1: {
+        //   text: 'Snack on Food.',
+        //   image: 'images/alcoholModule/alcohol_s23_02.jpg',
+        // },
+        // 2: {
+        //   text: 'Snack on Food.',
+        //   emoji: '🧐',
+        // },
+        src: 'images/alcoholModule/alcohol_s23_01.jpg',
+        emoji: '🍿'
+      }
+    }
+  },
   methods: {
     applyTest () {
-      let nodeId = 'list-list'
-      let style = document.getElementById(nodeId)
-      if (!style) {
-        style = document.createElement('style')
-        style.id = nodeId
-        style.type = 'text/css'
-        this.$el.appendChild(style)
-      }
+      let nodeId = 'head'
+      let getHeadTag = document.querySelector(nodeId)
+      console.log(getHeadTag)
+      let style = document.createElement('style')
+      getHeadTag.append(style)
+      style.type = 'text/css'
       let cssRule = `
-                li:before { content: '';
-                display: inline-block;
-                height: 1.25rem;
-                width: 1.25rem;
-                background-image: url("https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png");
-                background-size: cover;`
+        li:before { content: '` + this.photoList.emoji + `';
+        display: inline-block;
+        height: 1.25rem;
+        width: 1.25rem;
+        background-size: cover;`
       style.innerHTML = cssRule
+
+      // if (!style) {
+      //   style.id = nodeId;
+      //   style.type = 'text/css'
+      //   this.$el.appendChild(style)
+      // }
     }
   },
   mounted: function () {
@@ -36,9 +61,3 @@ export default {
   }
 }
 </script>
-
-<style>
-    li {
-        list-style: none;
-    }
-</style>
