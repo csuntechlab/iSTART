1d09e91c0ff751592c2989bd97db20738dc87b52
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-07-31 01:26:11 -0700

    Updated example modules

--- resources/js/components/modules/data/moduleSelector.js
+++ resources/js/components/modules/data/moduleSelector.js
@@ -17,9 +17,8 @@ export const moduleSelector = {
             status: this.checkCompletionStatus(0),
             date: '4 days left to complete'
           },
-          description: 'Alcohol, I feel fuzzy'
+          description: 'Alcohol'
         },
-
         marijuana: {
           name: 'Marijuana',
           image: 'images/thumbnail/marijuana_thumbnail.jpg',
@@ -30,9 +29,8 @@ export const moduleSelector = {
             status: this.checkCompletionStatus(50),
             date: 'Available until: 00/00/00'
           },
-          description: 'Marijuana; ...'
+          description: 'Marijuana'
         },
-
         tobacco: {
           name: 'Tobacco',
           image: 'images/thumbnail/nicotine_thumbnail.jpg',
@@ -43,7 +41,31 @@ export const moduleSelector = {
             status: this.checkCompletionStatus(100),
             date: 'Completed on: 00/00/00'
           },
-          description: 'Tobacco; I feel relaxed'
+          description: 'Tobacco'
+        },
+        prescriptionDrugs: {
+          name: 'Prescription Drugs',
+          image: 'images/thumbnail/prescription-drugs_thumbnail.jpg',
+          altText: 'prescription drugs',
+          completion: {
+            progress: 100,
+            completed: true,
+            status: this.checkCompletionStatus(100),
+            date: 'Completed on: 00/00/00'
+          },
+          description: 'Prescription Drugs'
+        },
+        illicitDrugs: {
+          name: 'Illicit Drugs',
+          image: 'images/thumbnail/illicit-drugs_thumbnail.jpg',
+          altText: 'illicit drugs',
+          completion: {
+            progress: 100,
+            completed: true,
+            status: this.checkCompletionStatus(100),
+            date: 'Completed on: 00/00/00'
+          },
+          description: 'Illicit Drugs'
         }
       }
     }
