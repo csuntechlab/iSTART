4e115698b210a1897daaba29d9cb7a522ad6db4f
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-08-06 10:11:19 -0700

    Finished second portion of nicotine module

--- resources/js/components/modules/data/modules/nicotineModule.js
+++ resources/js/components/modules/data/modules/nicotineModule.js
@@ -324,5 +324,407 @@ export const nicotineModuleSlides = {
       tooltip: 'Wait 5 seconds to proceed to the next slide'
     }
   },
+  // slide 19
+  11: {
+    slide_type: 'info',
+    header: {
+      title: 'Don\'t be misled!',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'College students often assume that the majority of their peers smoke or vape.',
+          1: 'However, less than 5% of students reported vaping in the last month.',
+          2: 'Don\'t be misled—these types of assumptions often lead students to smoke or vape in an attempt to fit in with their peers.'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s19_01.jpg',
+          alt: 'sitting in the park',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      }
+    }
+  },
+  // slide 20
+  11: {
+    slide_type: 'info',
+    header: {
+      title: 'Be Aware!',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Companies target young adults and teens with cool advertising to make vaping look appealing. Don\'t be tricked into vaping just to create an image!'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s20_01.jpg',
+          alt: 'no nicotine infographic',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      },
+      2: {
+        paragraph: {
+          0: 'While vape pens might look cool, they mask how much nicotine we consume. Some can actually deliver nicotine faster than regular cigarettes. This means that we may be ingesting more nicotine and chemicals while vaping!'
+        }
+      },
+    }
+  },
+  // slide 21 & 22 survey?
+  // slide 23
+  12: {
+    slide_type: 'info',
+    header: {
+      title: 'Don\'t get stuck in the cycle',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Another reason not to start smoking/vaping is that it\'s so hard to stop! Nicotine dependence results in strong cravings and many unpleasant withdrawal symptoms. Most smokers have to try several times to quit.'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s23_01.png',
+          alt: 'nicotine cycle',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      }
+    }
+  },
+  // slide 24
+  13: {
+    slide_type: 'info',
+    header: {
+      title: 'The truth hurts!',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'For years we\'ve heard the truth about the dangers of smoking cigarettes and second hand smoke: lung cancer, heart disease, stroke, to name a few.',
+          1: 'Now there\'s new research showing the harmful effects of e-cigs and vaping on brain development, the body, and so much more! Know the facts to make healthy choices and help those you love.'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s24_01.jpg',
+          alt: 'truth scrabble',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      }
+    }
+  },
+  // slide 25 ???
+  // slide 26 card flip
+  // slide 27
+  14: {
+    slide_type: 'info',
+    header: {
+      title: 'Sleep for Success',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'It\'s hard enough to catch some zzz\'s while in college; keep your head clear so you can sleep for success! Nicotine is a stimulant that can cause insomnia and withdrawal symptoms similar to caffeine.'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s27_01.png',
+          alt: 'sleep',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      },
+      2: {
+        paragraph: {
+          0: 'Nicotine use may also create other sleep interruptions. Research has shown that nicotine-users spend more time sleeping lightly and less time in deep sleep than non-users.'
+        }
+      },
+    }
+  },
+  // slide 28 card flip
+  // slide 29 time line card flip
+  // slide 30 
+  15: {
+    slide_type: 'info',
+    header: {
+      title: 'How to say "No"',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Saying "no" to tobacco may be tough, but it\'s important for your health and the health of those around you!',
+          1: 'Remember that you don\'t need to vape/smoke to be cool or fit in, real friends like you for who you are. Stand tall, hold your head high and speak with confidence!'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s30_01.jpg',
+          alt: 'offered cigarrette',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      },
+      2: {
+        list_element: [
+          '"No thanks, I don\'t like the taste"',
+          '"I\'ll pass. I\'ve had friends/family suffer from smoking-related diseases"',
+          '"I know people who have had a really hard time quitting"',
+          '"No thanks - I don\'t know what\'s in it"',
+          '"I don\'t want to be exposed to the toxins"',
+          '"I suffer from asthma"'
+        ]
+      }
+    }
+  },
+  // slide 31
+  16: {
+    slide_type: 'info',
+    header: {
+      title: 'CSUN Clear the Air!',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Those who smoke also put others at risk by exposing them to secondhand smoke! The only way to fully protect everyone is to eliminate smoking in all places.'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s31_01.png',
+          alt: 'csun clean air',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      },
+      2: {
+        paragraph: {
+          0: 'Smoke and tobacco products (e-cigs and vape pens!) are not allowed anywhere on CSUN\'s campus; not even in the parking lots or near your favorite picnic table.',
+          1: 'CA law also prohibits smoking inside any public building and within 20 feet of any building, anywhere in CA!'
+        }
+      },
+    }
+  },
+  // slide 32
+  17: {
+    slide_type: 'info',
+    header: {
+      title: 'Healthy Alternatives',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Sometimes we use tobacco/nicotine for social reasons, to help cope with stress and anxiety. But smoking/vaping isn\'t the only way to relieve stress or help us tackle life\'s challenges!',
+          1: 'There are many ways to boost health and wellness. Research shows physical, mental and social activities can improve overall wellness (check out the iSTART website for resources!).'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s32_01.jpg',
+          alt: 'cliffside',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      }
+    }
+  },
+  // slide 33
+  18: {
+    slide_type: 'info',
+    header: {
+      title: 'Shake it off!',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Even short periods of physical activity, especially aerobic exercise, reduce the urge to smoke or vape. Shaking it through running, dancing, cycling, or boxing can help.'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s33_01.png',
+          alt: 'dancing',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      },
+      2: {
+        paragraph: {
+          0: 'Exercise decreases appetite and helps limit the weight gain some people have when they quit smoking. Exercise also helps to cope with stress, creates more energy, and can improve mood!'
+        }
+      },
+      3: {
+        image: {
+          src: 'images/nicotineModule/nic_s33_02.jpg',
+          alt: 'boxing',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      },
+      4: {
+        paragraph: {
+          0: 'Did you know the Student Recreation Center offers A Boxing Training Program to students for FREE?'
+        }
+      },
+    }
+  },
+  // slide 34
+  19: {
+    slide_type: 'info',
+    header: {
+      title: 'Let off some steam – by talking!',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Do you have someone you trust that you can talk to? In order to live a healthy life, we all need connections with people we trust and feel safe around.',
+          1: 'Especially during stressful times, talking with someone and hearing supportive words can be just what we need. It can help us calm down and see things in a different light.',
+          2: '…and if you\'re craving a cigarette, invite your friend for a cup of tea instead!'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s34_01.png',
+          alt: 'lunch table',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      },
+      2: {
+        paragraph: {
+          0: 'If you feel like talking to a professional, you can contact University Counseling Services to request an appointment. See more info in the app menu.'
+        }
+      }
+    }
+  },
+  // slide 35
+  20: {
+    slide_type: 'cardFlip',
+    header: {
+      title: 'Healthy Inhale!',
+      text: null,
+      tooltip: 'Flip all cards to proceed to the next slide'
+    },
+    content: {
+      cards_flipped: false,
+      card_flip_count: 0,
+      paragraph: {
+        0: 'Deep breathing, or belly breathing, can reduce stress and anger, improve relaxation and help in overcoming fears.',
+        1: 'It can help you reach a state of deep rest. Air comes in through your nose and fully fills your lungs, and the lower belly rises. This type of breathing can slow the heartbeat and help stabilize blood pressure.',
+        2: '<b>Flip the card to learn how to belly breathe</b>'
+      },
+      cards: {
+        0: {
+          show: false,
+          front: {
+            img: {
+              src: 'images/nicotineModule/nic_s35_01.png',
+              alt: 'belly breathing',
+              caption: null
+            },
+            question: null
+          },
+          back: {
+            description: '<ol style="text-align:left;"><li>Find a quiet, comfortable place to sit or lie down (loosen tight clothing around your abdomen)</li> <li>First, take a normal breath. Then, a deep breath:</li> <li>Steadily & slowly inhale through your nose, filling your belly with air before filling the lungs.</li> <li>Hold this breath for a few seconds., then exhale all your air through the mouth; slowly and steadily as if you are blowing bubbles.</li> <li>Repeat, focusing your mind on your breathing.</li></ol> It might be strange or difficult at first, but practice gets you used to it! Try creating a daily routine.'
+          }
+        }
+      }
+    }
+  },
+  // slide  36
+  21: {
+    slide_type: 'emailForm',
+    header: {
+      title: 'Set a Wellness Goal',
+      text: 'Now is a great time to set a wellness goal for reducing stress or anxiety! Or if you use tobacco products (cigs, vape pens, etc.), what next steps can you take to reduce your nicotine use?',
+      tooltip: null
+    },
+    content: {
+      textAreaPlaceHolder: 'For example, "The next time I feel stressed, I will talk with a trusted friend who can help me share my thoughts" Or, \n "Next week I will look at the resources for help on how to reduce vaping"'
+    }
+  },
+  // slide 37
+  19: {
+    slide_type: 'info',
+    header: {
+      title: 'Resources',
+      text: null,
+      tooltip: 'Wait 5 seconds to proceed to the next slide'
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'If you are concerned about your own, or someone else\'s tobacco or nicotine use, you can find many resources for quitting in the app menu!'
+        }
+      },
+      1: {
+        image: {
+          src: 'images/nicotineModule/nic_s37_01.png',
+          alt: 'hands connecting',
+          mobile_size: '1/2',
+          tablet_size: '1/2',
+          desktop_size: '1/3'
+        }
+      }
+    }
+  },
+  // slide 38
+  23: {
+    slide_type: 'info',
+    header: {
+      title: null,
+      text: null
+    },
+    content: {
+      0: {
+        paragraph: {
+          0: 'Thank you for completing this module!',
+          1: 'You will receive an email notification from iSTART when the next module becomes available in 5 days. You will receive another gift card after you complete the next module!'
+        }
+      }
+    },
+    format: {
+      images: 'bottom'
+    }
+  }
 }
 export default nicotineModuleSlides
