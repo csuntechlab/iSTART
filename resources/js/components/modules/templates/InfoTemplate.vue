f5dc77b3338636617839612f660f3a130363cc7b
brianlinggadjaja <brianlinggadjaja@outlook.com>
2019-08-14 17:54:20 -0700

    Added conditional rendering for typography

--- resources/js/components/modules/templates/InfoTemplate.vue
+++ resources/js/components/modules/templates/InfoTemplate.vue
@@ -4,14 +4,14 @@
       <div class="col-12">
         <div class="row">
           <div class="col-12">
-            <h1 v-if="currentSlideData.header.header" class="module-info__header module-text__header">
+            <h1 v-if="currentSlideData.header.header && currentSlideData.header.header !== null" class="module-info__header module-text__header">
               {{ currentSlideData.header.header }}
             </h1>
           </div>
         </div>
         <div class="row">
           <div class="module-info__content col-12">
-            <h2 class="module-text__text" v-if="currentSlideData.header.sub_header" v-html="currentSlideData.header.sub_header"></h2>
+            <h2 class="module-text__text" v-if="currentSlideData.header.sub_header && currentSlideData.header.sub_header !== null" v-html="currentSlideData.header.sub_header"></h2>
           </div>
         </div>
         <template v-for="(element, currentSlideIndex) in currentSlideData.content">
