c6d0e20514387d510b7ed232fbc991da3c6bf6e9
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-09-04 11:36:41 -0700

    update the styling for the module footer

--- resources/js/components/modules/ModuleFooter.vue
+++ resources/js/components/modules/ModuleFooter.vue
@@ -1,22 +1,30 @@
 <template>
   <footer class="module-footer">
-    <span v-if="checkForStart()" class="module-footer__item-left module-footer__item">
-      <button @click="slideBack" class="module-footer__button button button-primary">Back</button>
-    </span>
-    <span v-if="checkForEnd()" class="module-footer__item-right module-footer__item">
-      <button v-if="isContinueButtonEnabled" @click="slideForward" class="module-footer__button button button-primary">
-        Continue
+    <div v-if="checkForStart()" class="module-footer__item-left module-footer__item">
+      <button @click="slideBack" class="module-footer__button button button-primary text-right text-md-center">
+        <i class="fas fa-chevron-left"></i>
+        Back
+      </button>
+    </div>
+    <div v-if="checkForEnd()" class="module-footer__item-right module-footer__item">
+      <button v-if="isContinueButtonEnabled" @click="slideForward" class="module-footer__button button button-primary text-left text-md-center">
+        Next
+        <!-- <i v-if="this.currentSlideNumber != this.totalSlides - 1" class="fas fa-chevron-right"></i> -->
       </button>
       <div v-else>
-        <div id="tooltip" class="module-footer__tooltip hidden">{{ currentSlideData.header.tooltip }}</div>
-        <button @click="showTooltip" @mouseover="showTooltip" @mouseout="hideTooltip" class="module-footer__button button button-primary--disabled">
-          Continue
+        <div id="tooltip" class="module-footer__tooltip hidden">
+          {{ currentSlideData.header.tooltip }}
+        </div>
+        <button @click="showTooltip" @mouseover="showTooltip" @mouseout="hideTooltip" class="module-footer__button button button-primary--disabled text-left text-md-center">
+          Next
         </button>
       </div>
-    </span>
-    <span v-else class="module-footer__item-right module-footer__item">
-      <button @click="returnToDashboard" class="module-footer__button button button-primary">Return to Dashboard</button>
-    </span>
+    </div>
+    <div v-else class="module-footer__item-right module-footer__item module-footer__item-return">
+      <button @click="returnToDashboard" class="module-footer__button button button-primary">
+        Return to Dashboard
+      </button>
+    </div>
   </footer>
 </template>
 
