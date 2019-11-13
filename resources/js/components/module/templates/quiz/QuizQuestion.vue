aea3fd9b6f1a366af1676e84b4893a88a742c0fa
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-11-13 11:51:10 -0800

    Made accessiblity improvements to the quiz template by utilizing buttons

--- resources/js/components/module/templates/quiz/QuizQuestion.vue
+++ resources/js/components/module/templates/quiz/QuizQuestion.vue
@@ -5,28 +5,28 @@
     </div>
     <div class="row">
       <div class="module-quiz__option-container col-sm-6 col-12" v-for="(element, optionIndex) in options" :key="optionIndex">
-        <div @click="showResponse(optionIndex)" :class="[(element.show && element.isAnswer ? 'module-quiz__option--correct' : '' ), (element.show && !element.isAnswer ? 'module-quiz__option--incorrect' : ''), 'module-quiz__option interactable-container']">
+        <button @click="showResponse(optionIndex)" :class="[(element.show && element.isAnswer ? 'module-quiz__option--correct' : '' ), (element.show && !element.isAnswer ? 'module-quiz__option--incorrect' : ''), 'module-quiz__option interactable-container']">
           <div v-if="object.showResponse" :class="[(object.options[optionIndex].show ? 'module-quiz__option-indicator--active' : ''), 'module-quiz__option-indicator']">
-            <div v-if="element.isAnswer" class="module-quiz__option-indicator-container bg-success">
+            <div v-if="element.isAnswer && element.show" class="module-quiz__option-indicator-container bg-success">
               <i class="fa fa-check-circle"></i>
             </div>
-            <div v-else class="module-quiz__option-indicator-container bg-danger">
+            <div v-else-if="!element.isAnswer && element.show" class="module-quiz__option-indicator-container bg-danger">
               <i class="fa fa-times-circle"></i>
             </div>
           </div>
 
           <div :class="[(object.options[optionIndex].show ? 'module-quiz__option-content--active' : ''), 'module-quiz__option-content']">
             <img v-if="element.img.src" :src="element.img.src" :alt="element.img.alt">
-            <p v-html="element.text"></p>
+            <p v-if="element.text" v-html="element.text"></p>
 
-            <div :class="[(element.show ? 'module-quiz__response--active' : ''), 'module-quiz__response']">
-              <p class="module-quiz__text" v-html="element.response.text"></p>
+            <div v-if="element.show" :class="[(element.show ? 'module-quiz__response--active' : ''), 'module-quiz__response']">
+              <p v-if="element.response.text" class="module-quiz__text" v-html="element.response.text"></p>
               <div class="module-quiz__response-image">
-                <img :src="element.response.img.src" :alt="element.response.img.alt">
+                <img v-if="element.response.img.src" :src="element.response.img.src" :alt="element.response.img.alt">
               </div>
             </div>
           </div>
-        </div>
+        </button>
       </div>
     </div>
   </div>
