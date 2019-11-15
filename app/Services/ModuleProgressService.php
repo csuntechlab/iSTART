6606ff83b7e60d61de8adf1382abd095a703f4af
luisjg <luis.guzman@metalab.csun.edu>
2019-11-15 10:27:53 -0800

    Add existence check.

--- app/Services/ModuleProgressService.php
+++ app/Services/ModuleProgressService.php
@@ -14,28 +14,31 @@ class ModuleProgressService implements ModuleProgressContract
 
     public function getModuleProgress($data): array
     {
+        $response = [];
         $moduleProgress = ModuleProgress::where('user_id',$data['user_id'])
                                             ->where('current_module',$data['current_module'])
-                                            ->first()
-                                            ->toArray();
-        
-        return [
-            'user_id' => $moduleProgress['user_id'],
-            'current_module' => $moduleProgress['current_module'],
-            'current_page' => $moduleProgress['current_page'],
-            'max_page' => $moduleProgress['max_page'],
-        ];
+                                            ->first();
+        if ($moduleProgress !== null) {
+            $moduleProgress->toArray();
+            $response = [
+                'user_id' => $moduleProgress['user_id'],
+                'current_module' => $moduleProgress['current_module'],
+                'current_page' => $moduleProgress['current_page'],
+                'max_page' => $moduleProgress['max_page'],
+            ];
+        }
+        return $response;
     }
 
     public function setModuleProgress($data)
     {
         $moduleProgress = ModuleProgress::updateOrCreate(
             [
-                'user_id' => $data['user_id'], 
+                'user_id' => $data['user_id'],
                 'current_module' => $data['current_module']
             ],
             [
-                'current_page' => $data['current_page'], 
+                'current_page' => $data['current_page'],
                 'max_page' => $data['max_page']
             ]
         );
