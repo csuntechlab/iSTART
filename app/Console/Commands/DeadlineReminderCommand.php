6d29da53c7df58fc19bc5344c615dec7075af37d
luisjg <contact@luisjg.io>
2019-11-23 16:17:10 -0800

    Complete the email reminder command.

--- app/Console/Commands/DeadlineReminderCommand.php
+++ app/Console/Commands/DeadlineReminderCommand.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\GenericEmail;
+use App\Models\User;
 
 class DeadlineReminderCommand extends Command
 {
@@ -11,14 +14,14 @@ class DeadlineReminderCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'istart:reminder';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Handles the reminder logic.';
 
     /**
      * Create a new command instance.
@@ -37,6 +40,22 @@ class DeadlineReminderCommand extends Command
      */
     public function handle()
     {
-        //
+        // Let's get the users that only have an actual Module
+        $users = User::with(['moduleProgress' => function ($q) {
+            $q->orderBy('created_at', 'DESC')->first();
+        }])->whereHas('moduleProgress')->get();
+        // get calls always return something
+        if (!empty($users)) {
+            foreach ($users as $user) {
+                if (!empty($user->moduleProgress)) {
+                    $currentModule = $user->moduleProgress->first();
+                    $dayCheck = $currentModule->created_at->diffInDays($currentModule->expires_at);
+                    if ($dayCheck == 2 || $dayCheck == 1) {
+                        // send out the email.
+                        Mail::to((env('RECIEVE_EMAIL')))->send(new GenericEmail($user));
+                    }
+                }
+            }
+        }
     }
 }
