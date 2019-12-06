8e80de6e965de431b684823b72a7d67910d7bec7
luisjg <luis.guzman@metalab.csun.edu>
2019-12-06 15:29:09 -0800

    Fix a few workflow issues.

--- app/Services/ModuleProgressService.php
+++ app/Services/ModuleProgressService.php
@@ -18,15 +18,17 @@ class ModuleProgressService implements ModuleProgressContract
             'user_id' => $data['user_id'],
             'current_module' => '',
             'current_page' => 0,
-            'max_page' => ''
+            'max_page' => '',
+            'expiration_date' => null
         ];
-        $moduleProgress = ModuleProgress::where('user_id',$data['user_id'])
+        $moduleProgress = ModuleProgress::where('user_id',$data['user_id'])->orderBy('creaded_at', 'DESC')
                                             ->first();
         if ($moduleProgress !== null) {
             $response['user_id'] = $moduleProgress->user_id;
             $response['current_module'] = $moduleProgress->current_module;
             $response['current_page'] = $moduleProgress->current_page;
             $response['max_page'] = $moduleProgress->max_page;
+            $response['expiration_date'] = $moduleProgress->expiration_date;
         }
         return $response;
     }
@@ -49,7 +51,7 @@ class ModuleProgressService implements ModuleProgressContract
     public function moduleComplete($data){
 
         $user = $data['user_id'];
-        $moduleComplete = ModuleProgress::find($user);
+        $moduleComplete = ModuleProgress::where('user_id', $data['user_id'])->where('current_module', $data['current_module'])->first();
 
         if($moduleComplete == null){
 
@@ -59,7 +61,31 @@ class ModuleProgressService implements ModuleProgressContract
         $job = (new SendNewModuleEmail($moduleComplete))
             ->delay(Carbon::now()->addSeconds(env('MODULE_COMPLETION')));
             dispatch($job);
+        $moduleComplete->expiration_date = null;
+        $moduleComplete->touch();
+        $moduleComplete->save();
 
+    }
+
+    public function createNewModule($data)
+    {
+        $moduleProgress = ModuleProgress::create([
+            'user_id' => $data['user_id'],
+            'current_module' => '',
+            'current_page' => 0,
+            'max_page' => 0,
+            'expiration_date' => Carbon::now()->addDays(config('app.days_to_expire'))->toDateTimeString(),
+        ]);
+        return $moduleProgress;
+    }
+
+    public function moduleExists($data)
+    {
+        $moduleProgress = ModuleProgress::where('user_id', $data['user_id'])->whereNotNull('expiration_date')->orderBy('created_at', 'DESC')->first();
+        if ($moduleProgress === null) {
+            return false;
+        }
+        return true;
     }
 
 }
