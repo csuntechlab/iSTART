c4e7b5bf19e6af74eff46897e0da5f1b4ec3629a
luisjg <luis.guzman@metalab.csun.edu>
2019-12-10 15:11:46 -0800

    Remove not null check.

--- app/Services/ModuleProgressService.php
+++ app/Services/ModuleProgressService.php
@@ -38,7 +38,7 @@ class ModuleProgressService implements ModuleProgressContract
     {
         $moduleProgress = ModuleProgress::where('user_id', $data['user_id'])
         ->where('current_module', $data['current_module'])
-        ->whereNotNull('completed_at')
+        ->whereNull('completed_at')
         ->first();
 
         if ($moduleProgress === null) {
@@ -61,7 +61,6 @@ class ModuleProgressService implements ModuleProgressContract
 
     public function moduleComplete($data)
     {
-        $user = $data['user_id'];
         $moduleComplete = ModuleProgress::where('user_id', $data['user_id'])
         ->where('current_module', $data['current_module'])
         ->whereNotNull('completed_at')
