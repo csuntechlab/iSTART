3c98845e13d28fc06130d20a0b78c0c0f12df570
luisjg <luis.guzman@metalab.csun.edu>
2019-12-12 12:09:13 -0800

    Adjust get-module-progress to return all of the modules.

--- resources/views/mail/UserCompletesModuleEmailToAdmin.blade.php
+++ resources/views/mail/UserCompletesModuleEmailToAdmin.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
     Hello Admin,
     <br>
-    The following student: {{$userCompletesModuleEmailToAdmin->student}} has
+    The following student: {{$userCompletesModuleEmailToAdmin->student->getUserGroup()->display_name}} has
     completed the {{ ucfirst($userCompletesModuleEmailToAdmin->current_module)}} module.
