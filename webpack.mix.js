45c4227796642c5f6a2a37c66750289556162a6c
luisjg <contact@luisjg.io>
2020-01-18 21:01:22 -0800

    Actually import fontawesome.

--- webpack.mix.js
+++ webpack.mix.js
@@ -27,6 +27,8 @@ const mix = require('laravel-mix')
 
 mix.js('resources/js/main.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
+
+// mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
 if (mix.inProduction()) {
   mix.version()
 }
