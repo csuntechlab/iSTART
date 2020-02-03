e84b83113c50e571963744a2bfe6ab1a8119e9a2
luisjg <luis.guzman@metalab.csun.edu>
2020-02-03 10:30:31 -0800

    Only allow students to be included in the submission.

--- app/ModelRepositories/UserModelRepository.php
+++ app/ModelRepositories/UserModelRepository.php
@@ -7,17 +7,25 @@ namespace App\ModelRepositories;
 use App\ModelRepositoryInterfaces\UserModelRepositoryInterface;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UserModelRepository implements UserModelRepositoryInterface
 {
     public function find($email)
     {
+        $status = Str::contains($email, '@csun.edu');
+
+        if ($status) {
+            return null;
+        }
+
         $user = User::where('email', $email)->first();
-            if(null === $user) {
-                return null;
-            } else {
-                $user_id = $user->toArray()['user_id'];
-                return $user_id;
-            }
+
+        if(null === $user) {
+            return null;
+        } else {
+            $user_id = $user->toArray()['user_id'];
+            return $user_id;
+        }
     }
 }
