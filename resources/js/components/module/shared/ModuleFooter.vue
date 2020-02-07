2091f70069f3ff9182ccd9e4c4d4cf2ffc09a952
luisjg <contact@luisjg.io>
2020-02-06 21:18:45 -0800

    Modify the check and module complete logic.

--- resources/js/components/module/shared/ModuleFooter.vue
+++ resources/js/components/module/shared/ModuleFooter.vue
@@ -78,21 +78,60 @@ export default {
     ),
 
     checkForEnd () {
-      let currentSlideData = this.getCurrentSlideData
-      let currentSlideType = currentSlideData.slide_type
-
-      if (currentSlideType === 'finalSlide' && !this.checkForReviewMode()) {
-        let isUserIntervention = (this.user.user_group === 'intervention')
-
-        // If user is intervention group, pass info to back-end on completed module
-        if (isUserIntervention) {
-          this.sendModuleCompleted()
+      // let's check to see if we're at the end of the slide
+      if (this.currentSlideNumber === (this.totalSlides - 1)) {
+        let userId = this.user.user_id
+        let currentModule = this.getCurrentModule.toLowerCase()
+        // let's check to see if we're on comparison
+        if (this.getCurrentModuleData.group === 'comparison') {
+          // let's mark the module as finished
+          let completePayload = {
+            user_id: userId,
+            current_module: currentModule,
+            next_module: null,
+            index: 5
+          }
+          this.completeModule(completePayload)
+          return true
+        } else {
+          // we are not in comparison we are in intervention.
+          let moduleData = this.getModuleData
+          // remove the last index since that one is comparison
+          moduleData.pop()
+          let moduleDataLength = Object.keys(moduleData).length
+          for (let i = 0; i < moduleDataLength; i++) {
+            let moduleDataName = moduleData[i].name.toLowerCase()
+            let moduleDataIsReview = moduleData[i].progress.is_review
+            if ((moduleDataName === currentModule) && !moduleDataIsReview) {
+              if ((i + 1) < moduleDataLength) {
+                let nextModule = moduleData[i + 1].name.toLowerCase()
+                if ((i !== moduleDataLength) && (this.user.user_group === 'intervention')) {
+                  let completePayload = {
+                    user_id: userId,
+                    current_module: currentModule,
+                    next_module: nextModule,
+                    index: i
+                  }
+                  this.completeModule(completePayload)
+                  return true
+                }
+              } else {
+                // we finished the intervention modules
+                let completePayload = {
+                  user_id: userId,
+                  current_module: currentModule,
+                  next_module: null,
+                  index: i
+                }
+                this.completeModule(completePayload)
+                return true
+              }
+            }
+          }
         }
-
-        return true
-      } else {
-        return false
       }
+      // we are not on the last slide so let's proceed as usual
+      return false
     }
   },
 
@@ -149,25 +188,6 @@ export default {
         this.setModuleProgress(payload)
       }
     },
-
-    sendModuleCompleted () {
-      let moduleData = this.getModuleData
-      let moduleDataLength = Object.keys(moduleData).length
-
-      for (let i = 0; i < moduleDataLength; i += 1) {
-        let userId = this.user.user_id
-        let currentModule = this.getCurrentModule.toLowerCase()
-        let nextModule = moduleData[i + 1].name.toLowerCase()
-        let completePayload = {
-          user_id: userId,
-          current_module: currentModule,
-          next_module: nextModule,
-          index: i
-        }
-        this.completeModule(completePayload)
-      }
-    },
-
     checkForStart () {
       if (this.currentSlideNumber === 0) {
         this.enableContinue()
