53df37cc79fbe95c6d19571f3f3b15f3530e7746
luisjg <luis.guzman@metalab.csun.edu>
2020-03-02 07:46:35 -0800

    Fix login reminder.

--- app/Console/Commands/LoginReminderCommand.php
+++ app/Console/Commands/LoginReminderCommand.php
@@ -46,40 +46,35 @@ class LoginReminderCommand extends Command
         // Let's get the users that do not have a Module
         $users = User::with(['participant', 'moduleProgress', 'getUserGroup'])
             ->whereHas('participant')
-            ->whereHas('getUserGroup', function($q) {
-                $q->where('user_group', '!=', 'control');
-            })
             ->doesntHave('moduleProgress')
             ->get();
         // get calls always return something
         if (!empty($users)) {
             foreach ($users as $user) {
-                if (count($user->moduleProgress) === 0) {
-                    $today = Carbon::now(config('app.user_timezone'));
-                    $then = Carbon::parse($user->participant->created_at)->setTimezone(config('app.user_timezone'));
-                    $dayCheck = $today->diffInDays($then);
-                    if ($user->getUserGroup->user_group === 'intervention' || $user->getUserGroup === null) {
-                        if ($dayCheck === (config('app.days_to_expire') - 2) || $dayCheck === (config('app.days_to_expire') - 3)) {
-                            // send out the email.
-                            Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHasntLoggedInEmail($user));
-                        } else if ($dayCheck === (config('app.days_to_expire') - 1)) {
-                            Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHas24HoursLeftEmail($user));
-                        } else if ($dayCheck >= config('app.days_to_expire')) {
-                            // send out the student has been removed email.
-                            Mail::to(env('RECEIVE_EMAIL'))->send(new StudentRemovedFromStudyAdminEmail($user, null));
-                            $user->participant()->delete();
-                        }
-                    } else {
-                        if ($dayCheck === (30 / 2) || $dayCheck === (30 / 3) || $dayCheck === (30 / 6)) {
-                            // send out the email.
-                            Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHasntLoggedInEmail($user));
-                        } else if ($dayCheck === (30 - 1)) {
-                            Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHas24HoursLeftEmail($user));
-                        } else if ($dayCheck >= 30) {
-                            // send out the student has been removed email.
-                            Mail::to(env('RECEIVE_EMAIL'))->send(new StudentRemovedFromStudyAdminEmail($user, null));
-                            $user->participant()->delete();
-                        }
+                $today = Carbon::now(config('app.user_timezone'));
+                $then = Carbon::parse($user->participant->created_at)->setTimezone(config('app.user_timezone'));
+                $dayCheck = $today->diffInDays($then);
+                if ($user->getUserGroup === null || $user->getUserGroup->user_group === 'intervention') {
+                    if ($dayCheck === (config('app.days_to_expire') - 2) || $dayCheck === (config('app.days_to_expire') - 3)) {
+                        // send out the email.
+                        Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHasntLoggedInEmail($user));
+                    } else if ($dayCheck === (config('app.days_to_expire') - 1)) {
+                        Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHas24HoursLeftEmail($user));
+                    } else if ($dayCheck >= config('app.days_to_expire')) {
+                        // send out the student has been removed email.
+                        Mail::to(env('RECEIVE_EMAIL'))->send(new StudentRemovedFromStudyAdminEmail($user, null));
+//                        $user->participant()->delete();
+                    }
+                } else if($user->getUserGroup === 'comparison') {
+                    if ($dayCheck === (30 / 2) || $dayCheck === (30 / 3) || $dayCheck === (30 / 6)) {
+                        // send out the email.
+                        Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHasntLoggedInEmail($user));
+                    } else if ($dayCheck === (30 - 1)) {
+                        Mail::to($user->email)->cc(env('RECEIVE_EMAIL'))->send(new UserHas24HoursLeftEmail($user));
+                    } else if ($dayCheck >= 30) {
+                        // send out the student has been removed email.
+                        Mail::to(env('RECEIVE_EMAIL'))->send(new StudentRemovedFromStudyAdminEmail($user, null));
+//                        $user->participant()->delete();
                     }
                 }
             }
