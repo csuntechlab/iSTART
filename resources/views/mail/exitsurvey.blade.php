4ea1a37edc454b60b48241e6c2d0364061a8d8c3
luisjg <contact@luisjg.io>
2020-03-26 17:47:24 -0700

    Adjust the wording on the exit email.

--- resources/views/mail/exitsurvey.blade.php
+++ resources/views/mail/exitsurvey.blade.php
@@ -8,7 +8,7 @@
             <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px;">After you complete the survey, you will receive a $25 Amazon.com gift card via email. Access the survey below:</p>
             <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px;"> <a href="https://www.surveymonkey.com/r/Exit_Survey_Participants" target="_blank" rel="noreferrer" style="color: rgb(55, 113, 220); font-size: 14px; font-weight: bold; text-align: center; text-decoration: underline;">https://www.surveymonkey.com/r/Exit_Survey_Participants</a>.</p>
         @elseif($user->getUserGroup->user_group === 'comparison')
-            Thank you for your participation in the web-app study. You have now completed the web module! Please take the Exit survey now (it will take around 10 minutes).
+            Thank you for your participation in the web-app study. Please take the Exit survey now (it will take around 10 minutes).
             <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px;">After you complete the survey, you will receive a $15 Amazon.com gift card via email. Access the survey below:</p>
             <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px;"> <a href="https://www.surveymonkey.com/r/postcomparison" target="_blank" rel="noreferrer" style="color: rgb(55, 113, 220); font-size: 14px; font-weight: bold; text-align: center; text-decoration: underline;">https://www.surveymonkey.com/r/postcomparison</a>.</p>
         @else
