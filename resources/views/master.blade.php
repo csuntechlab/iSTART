ff6ed86fddb621980a8f37743041dc42a1357f21
luisjg <contact@luisjg.io>
2020-09-14 13:15:07 -0700

    Use the env value when ever possible.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -104,7 +104,7 @@ background-color: #106dbc !important; border-color: #106dbc !important; -webkit-
                                 <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
                                   <tbody>
                                     <tr>
-                                      <td style="font-family: sans-serif; font-size: 14px; border-radius: 5px;" align="center" bgcolor="3771DC" valign="top"> <a href="https://www.sandbox.csun.edu/apps/istart/#/login" target="_blank" style="color: #ffffff; text-decoration: none; background-color: rgb(55, 113, 220); border-radius: 5px; box-sizing: border-box; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; border: 1px solid rgb(55,113,220);">iStart Web App</a> </td>
+                                      <td style="font-family: sans-serif; font-size: 14px; border-radius: 5px;" align="center" bgcolor="3771DC" valign="top"> <a href="{{ url('/') }}" target="_blank" style="color: #ffffff; text-decoration: none; background-color: rgb(55, 113, 220); border-radius: 5px; box-sizing: border-box; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; border: 1px solid rgb(55,113,220);">iStart Web App</a> </td>
                                     </tr>
                                   </tbody>
                                 </table>
